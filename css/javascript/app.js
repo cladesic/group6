39fd771bbc693050e6d066b840fe232f5adb2328
Charles Ladesic <cladesic.student@gmail.com>
2019-03-24 19:33:36 -0500

    updated Modal form

--- css/javascript/app.js
+++ css/javascript/app.js
@@ -1,3 +1,4 @@
-//$(document).ready(function(){
- //   $('.modal').modal();
- /// });
+document.addEventListener('DOMContentLoaded', function() {
+   var elems = document.querySelectorAll('.modal');
+   var instances = M.Modal.init(elems, open);
+ });
