c47a5e6fc9762fa693bb3c4d4a51919de21e7aa6
KalPatel <kalpatel82@ymail.com>
2019-03-28 20:30:08 -0500

    frontend updates

--- css/javascript/app.js
+++ css/javascript/app.js
@@ -16,3 +16,19 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('#historyModal');
   var instances = M.Modal.init(elems, open);
 });
+
+// Slider on front page
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.slider');
+  var instances = M.Slider.init(elems, 100, 100, 200, 300);
+});
+
+
+
+$(document).ready(function(){
+  $('.slider').slider();
+});
+        
+// -----------End Slider--------------------
